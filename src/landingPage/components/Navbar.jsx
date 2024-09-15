c758dcd3184d301694646c02b5f936db28b12f49
aristi1215 <juanarieda@gmail.com>
2024-09-15 10:02:54 -0700

    ADD: responsive redesign finished

--- src/landingPage/components/Navbar.jsx
+++ src/landingPage/components/Navbar.jsx
@@ -1,6 +1,6 @@
 export const Navbar = () => {
   return (
-    <nav className="flex justify-between items-center h-[4rem] bg-[#191A23] relative px-2">
+    <nav className="flex justify-between items-center h-[4rem] bg-[#404040] relative px-2">
         <div className="flex items-center gap-4">
             <img className="w-10" src="/images/logo-nova.png" alt="" />
             <h1 className="text-white"><strong className="font-normal">Nova Web Design</strong></h1>
