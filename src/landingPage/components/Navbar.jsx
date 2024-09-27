7fc559a1b48a41fc3f42d9209722be004d06cd02
aristi1215 <juanarieda@gmail.com>
2024-09-27 11:18:48 -0700

    Add feat: Change of the logo and hide information boxes of products and our work sections in small screens and finally reduce of the text in the testimonials

--- src/landingPage/components/Navbar.jsx
+++ src/landingPage/components/Navbar.jsx
@@ -2,10 +2,9 @@ export const Navbar = () => {
   return (
     <nav className="flex justify-between items-center h-[4rem] bg-[#404040] relative px-2">
         <div className="flex items-center gap-4">
-            <img className="w-10" src="/images/logo-nova.png" alt="" />
+            <img className="w-10 rounded-full" src="/images/logo-nova.jpeg" alt="" />
             <h1 className="text-white"><strong className="font-normal">Nova Web Design</strong></h1>
         </div>
-        <img width={20} height={20} src="/Icons/three-lines.svg" alt="" />
     </nav>
   )
 }
